3e6936fe66d2c10d2c24fc019b8e9c375dc4a12e
DarekParodia <darekparodia@gmail.com>
2025-10-21 12:19:04 +0200

    logger (in progress)

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,9 +1,18 @@
 #include "core.h"
 
+#include "logger.h"
+
+#include <Arduino.h>
+
 namespace core {
     void init() {
+#if (USE_SERIAL)
+        Serial.begin(SERIAL_BAUD);
+        Logger::debug("Serial initialized");
+#endif
     }
 
     void loop() {
+        Logger::info("Loop, millis: %lu", millis());
     }
 } // namespace core
--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,10 @@
 
 #include <Arduino.h>
 
-// put function declarations here:
-int  myFunction(int, int);
-
 void setup() {
-    // put your setup code here, to run once:
-
-    Serial.begin(115200);
     core::init();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    int result = myFunction(2, 3);
-    Serial.print("result: ");
-    Serial.print(result);
-    Serial.print(" | milis: ");
-    Serial.println(millis());
-    delay(1000);
     core::loop();
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-    return x + y;
 }
